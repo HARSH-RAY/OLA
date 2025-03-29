77a088f249d062f5b2a6709e2ffc1bf130e542c0
HARSH-RAY <harshroy2637@gmail.com>
2025-03-29 09:14:58 +0530

    gretter_or_lesser

---
+++ ch_2.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+public class ch_2 {
+    
+        public static void main(String[] args) {
+        //input by user
+        System.out.println("your number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        //random to genrated to compared
+        Random rand = new Random();
+        int Guess = rand.nextInt(100) + 1;
+
+        //no is greater or not
+        if (num > Guess){
+            System.out.println("greater");
+        } else {
+            System.out.println("lesser");
+        }
+
+        sc.close();
+        }
+    
+}
